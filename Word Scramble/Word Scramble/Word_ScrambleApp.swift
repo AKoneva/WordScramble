f182089dd6994ffc636ab7b4db0c006693c522f4
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-08 17:45:07 +0900

    Add resources and game logic

--- Word Scramble/Word Scramble/Word_ScrambleApp.swift
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Word_ScrambleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
