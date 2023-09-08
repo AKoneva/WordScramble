bf0f454a29e8c0453517aaaf93ace78bb8490922
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-08 16:57:55 +0900

    init

---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Анна Перехрест  on 2023/09/08.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
